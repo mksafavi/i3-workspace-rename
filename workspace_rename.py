a07aa956d96504ebe6a7ee7e6a3b1400bc9e323a
Mk Safavi <mksafavi@gmail.com>
2023-02-26 09:19:29 +0330

    added missing type hints

--- workspace_rename.py
+++ workspace_rename.py
@@ -20,7 +20,7 @@ def rename_workspace(connection: Connection, rename_command: list[str]):
     )
 
 
-def get_rename_command(workspace, separator, max_length, window_property):
+def get_rename_command(workspace: Con, separator: str, max_length: int, window_property: WindowProperty) -> list[str]:
     windows_properties = _get_workspace_windows_properties(workspace=workspace)
     windows_names = _get_windows_names(windows_properties=windows_properties, window_property=window_property, max_length=max_length)
     new_name = _create_workspace_name(windows_names=windows_names, separator=separator)
@@ -36,7 +36,7 @@ def _get_workspace_windows_properties(workspace: Iterable[Con]) -> Generator:
     )
 
 
-def _get_windows_names(windows_properties: Iterable[tuple], window_property: WindowProperty, max_length: int) -> list:
+def _get_windows_names(windows_properties: Iterable[tuple], window_property: WindowProperty, max_length: int) -> list[str]:
     return [
         _truncate_string(window[window_property.value], max_length=max_length)
         for window in windows_properties
@@ -47,7 +47,7 @@ def _create_workspace_name(windows_names: list, separator: str = ' ') -> str:
     return separator.join(windows_names)
 
 
-def _create_rename_command(number: int, name: str, new_name: str) -> list:
+def _create_rename_command(number: int, name: str, new_name: str) -> list[str]:
     return ['rename', 'workspace', f'"{_escape_double_quote(name)}"', 'to',
             f'"{number}: {_escape_double_quote(new_name)}"']
 
